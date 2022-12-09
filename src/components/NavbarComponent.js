3d00d18f157e8ca71243a31628a6360be60b9bdf
Camilo Rozo <55302468+Cerozob@users.noreply.github.com>
2022-12-08 20:28:18 -0500

    final

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,40 +1,41 @@
 import React from 'react';
-import { Navbar, Nav, Container, Row, Card } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import '../pages/App.css';
+import { useLocation } from 'react-router-dom';
 
 function NavbarComponent() {
+  const location = useLocation();
+  const path = location.pathname;
+
+  let navPages = [
+    { className: "nav-link", to: "/home", display: "Introducción" },
+    { className: "nav-link", to: "/ayc", display: "Arte & Ciencia" },
+    { className: "nav-link", to: "/case", display: "Caso de estudio" },
+    { className: "nav-link", to: "/about", display: "Desarrollo del producto" },
+    { className: "nav-link", to: "/refs", display: "Referencias" }
+  ];
+
+
   return (
     <Navbar bg="#21222c" expand="lg" sticky="top">
       <Container id="navContainer">
         <Navbar.Toggle id="navbarbutton" aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto" >
-            <Link className="nav-link" to="/home">Introducción</Link>
-            <Link className="nav-link" to="/ayc">{"Arte & Ciencia"}</Link>
-            <Link className="nav-link" to="/case">Caso de estudio</Link>
-            <Link className="nav-link" to="/about">Desarrollo del producto</Link>
-            <Link className="nav-link" to="/refs">Referencias</Link>
+            {navPages.map((page) => {
+              if (page.to === path) {
+                page.className += "-selected";
+              }
+              return (
+                <Link to={page.to} className={page.className} key={page.to}>
+                  {page.display}
+                </Link>
+              )
+            })}
           </Nav>
         </Navbar.Collapse>
       </Container>
-      {/* <Row>
-         <Card border="warning"
-           bg={"danger"}
-           key={"danger"}
-           text={'dark'}
-           style={{ width: '90rem' }}
-           className="mb-2">
-           <Card.Body>
-             <Card.Title style={{ color: "black", textAlign: "center" }}>Aclaración - Sitio no terminado</Card.Title>
-             <Card.Text style={{ color: "black", textAlign: "center" }}>
-               Este sitio continuará cambiando hasta la fecha de entrega del producto (2022-Jun-07)
-             </Card.Text>
-           </Card.Body>
-         </Card>
-         <br />
-   
-       </Row> */}
     </Navbar>
 
   );
--- src/pages/Refs.js
+++ src/pages/Refs.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Col, Container, Row, Card } from 'react-bootstrap';
-import googlefonts from "../assets/googlefonts.png";
-import gizmodo from "../assets/gizmodo.svg";
-import wired from "../assets/wired.svg";
-import neurallove from "../assets/neurallove.svg";
-import nvidia from "../assets/nvidia.svg";
+import GoogleFontsLogo from "../assets/googlefonts.png";
+import GizmodoLogo from "../assets/gizmodo.svg";
+import WiredLogo from "../assets/wired.svg";
+import NeuralLoveLogo from "../assets/neurallove.svg";
+import NvidiaLogo from "../assets/nvidia.svg";
 import NavbarComponent from '../components/NavbarComponent';
 
 export default function Refs() {
+
     return (
         <div>
             <NavbarComponent></NavbarComponent>
@@ -17,10 +18,11 @@ export default function Refs() {
                 <Row xs={1} md={2} xxl={4}>
 
                     <Col className="refcol">
-                        <Card className="mainCard">
+                        <Card className="mainCard refCard">
                             <a target="blank" href="https://neural.love/">
-                                <Card.Title><h3>neural.love - Sitio</h3></Card.Title>
-                                <Card.Img className="refimage" variant="bottom" src={neurallove} id="neuralovelogo" /></a>
+                                <Card.Title><h3 className="ref-link">neural.love - Sitio</h3></Card.Title>
+                                <Card.Img className="refimage" variant="bottom" src={NeuralLoveLogo} id="neuralovelogo" />
+                            </a>
                             <Card.Body className="maincardContent">
                                 <p>El sitio principal donde se puede probar la tecnología desarrollada por la empresa del mismo nombre. Múltiples secciones de contenido se generaron a partir del contenido de este sitio.
                                 </p>
@@ -30,10 +32,10 @@ export default function Refs() {
                     </Col>
 
                     <Col className="refcol">
-                        <Card className="mainCard">
+                        <Card className="mainCard refCard">
                             <a target="blank" href="https://neural.love/about">
-                                <Card.Title><h3>neural.love - Responsables</h3></Card.Title>
-                                <Card.Img className="refimage" variant="bottom" src={neurallove} id="neuralovelogo" /></a>
+                                <Card.Title><h3 className="ref-link">neural.love - Responsables</h3></Card.Title>
+                                <Card.Img className="refimage" variant="bottom" src={NeuralLoveLogo} id="neuralovelogo" /></a>
                             <Card.Body className="maincardContent">
                                 <p>
                                     Subsección del sitio con información resumida sobre los creadores de esta empresa.
@@ -44,10 +46,10 @@ export default function Refs() {
 
 
                     <Col className="refcol">
-                        <Card className="mainCard">
+                        <Card className="mainCard refCard">
                             <a href="https://www.youtube.com/c/DenisShiryaev" target="blank">
-                                <Card.Title><h3>Denis Shiryaev - Autor</h3></Card.Title>
-                                <Card.Img className="refimage" variant="bottom" src={neurallove} id="neuralovelogo" /></a>
+                                <Card.Title><h3 className="ref-link">Denis Shiryaev - Autor</h3></Card.Title>
+                                <Card.Img className="refimage" variant="bottom" src={NeuralLoveLogo} id="neuralovelogo" /></a>
                             <Card.Body className="maincardContent">
                                 <p>
                                     Denis Shiryaev es el CEO de neural.love. Los videos de demostración, que se usaron en este sitio, están disponibles en su canal de YouTube.
@@ -72,10 +74,10 @@ export default function Refs() {
 
 
                     <Col className="refcol">
-                        <Card className="mainCard">
+                        <Card className="mainCard refCard">
                             <a target="blank" href="https://gizmodo.com/neural-networks-upscale-film-from-1896-to-4k-make-it-l-1841449875">
-                                <Card.Title><h3>Gizmodo - Artículo</h3></Card.Title>
-                                <Card.Img className="refimage" variant="bottom" src={gizmodo} /></a>
+                                <Card.Title><h3 className="ref-link">Gizmodo - Artículo</h3></Card.Title>
+                                <Card.Img className="refimage" variant="bottom" src={GizmodoLogo} /></a>
                             <Card.Body className="maincardContent">
                                 <p>
                                     Uno de los artículos usado en este sitio.
@@ -86,10 +88,10 @@ export default function Refs() {
 
 
                     <Col className="refcol">
-                        <Card className="mainCard">
+                        <Card className="mainCard refCard">
                             <a target="blank" href="https://www.wired.com/story/ai-magic-makes-century-old-films-look-new/">
-                                <Card.Title><h3>Wired - Artículo</h3></Card.Title>
-                                <Card.Img className="refimage" variant="bottom" src={wired} /></a>
+                                <Card.Title><h3 className="ref-link">Wired - Artículo</h3></Card.Title>
+                                <Card.Img className="refimage" variant="bottom" src={WiredLogo} /></a>
                             <Card.Body className="maincardContent">
                                 <p>
                                     Un segundo artículo usado en este sitio, muy similar al anterior.
@@ -100,10 +102,10 @@ export default function Refs() {
 
 
                     <Col className="refcol">
-                        <Card className="mainCard">
+                        <Card className="mainCard refCard">
                             <a target="blank" href="https://blogs.nvidia.com/blog/2020/10/23/ai-startup-enhances-video/">
-                                <Card.Title><h3>Nvidia - Artículo</h3></Card.Title>
-                                <Card.Img className="refimage" variant="bottom" src={nvidia} /></a>
+                                <Card.Title><h3 className="ref-link">Nvidia - Artículo</h3></Card.Title>
+                                <Card.Img className="refimage" variant="bottom" src={NvidiaLogo} /></a>
                             <Card.Body className="maincardContent">
                                 <p>
                                     Artículo de Nvidia. Se parafrasea la sección dónde hablan de realizar el trabajo de forma manual.
@@ -113,10 +115,10 @@ export default function Refs() {
                     </Col>
 
                     <Col className="refcol">
-                        <Card className="mainCard">
+                        <Card className="mainCard refCard">
                             <a target="blank" href="https://neural.love/upscale">
-                                <Card.Title><h3>neural.love - Reescalado</h3></Card.Title>
-                                <Card.Img className="refimage" variant="bottom" src={neurallove} id="neuralovelogo" /></a>
+                                <Card.Title><h3 className="ref-link">neural.love - Reescalado</h3></Card.Title>
+                                <Card.Img className="refimage" variant="bottom" src={NeuralLoveLogo} id="neuralovelogo" /></a>
                             <Card.Body className="maincardContent">
                                 <p>
                                     Subsección del sitio principal donde explican cómo funciona el reescalado de imágenes para crear multimedia de mayor resolución.
@@ -126,10 +128,10 @@ export default function Refs() {
                     </Col>
 
                     <Col className="refcol">
-                        <Card className="mainCard">
+                        <Card className="mainCard refCard">
                             <a target="blank" href="https://neural.love/blog/how-to-restore-and-enhance-100-year-old-movie-with-ai#step-1-restore-actual-fps-by-removing-duplicated-frames-automatically">
-                                <Card.Title><h3>neural.love - procedimiento</h3></Card.Title>
-                                <Card.Img className="refimage" variant="bottom" src={neurallove} id="neuralovelogo" /></a>
+                                <Card.Title><h3 className="ref-link">neural.love - procedimiento</h3></Card.Title>
+                                <Card.Img className="refimage" variant="bottom" src={NeuralLoveLogo} id="neuralovelogo" /></a>
                             <Card.Body className="maincardContent">
                                 <p>
                                     Subsección del sitio principal donde se explican las consideraciones a tener en cuenta durante el procedimiento de mejora de multimedia.
@@ -139,11 +141,11 @@ export default function Refs() {
                     </Col>
 
                     <Col className="refcol">
-                        <Card className="mainCard">
+                        <Card className="mainCard refCard">
                             <a target="blank" href="https://fonts.google.com/icons?selected=Material+Icons&icon.style=Rounded&icon.set=Material+Icons">
 
-                                <Card.Title><h3>Google - íconos</h3></Card.Title>
-                                <Card.Img className="refimage" variant="bottom" src={googlefonts} /></a>
+                                <Card.Title><h3 className="ref-link">Google - íconos</h3></Card.Title>
+                                <Card.Img className="refimage" variant="bottom" src={GoogleFontsLogo} /></a>
                             <Card.Body className="maincardContent">
                                 <p>Los íconos usados en este sitio fueron tomados de aquí</p>
                             </Card.Body>
